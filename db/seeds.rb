0001d11e9ef5fc53cfd829ab4569339c12466565
Bastien Grandjean <bastien.grandjean.75@gmail.com>
2022-05-18 14:35:21 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: 'Le bon resto', phone_number: '0102030405', address: 'Rue de Paris', category: 'french')
+Restaurant.create(name: 'La bonne frite', phone_number: '0102030405', address: 'Rue de Bruxelles', category: 'belgian')
+Restaurant.create(name: 'Le bon nem', phone_number: '0102030405', address: 'Rue de Pekin', category: 'chinese')
+Restaurant.create(name: 'La bonne pizza', phone_number: '0102030405', address: 'Rue de Venise', category: 'italian')
+Restaurant.create(name: 'Le bon sushi', phone_number: '0102030405', address: 'Rue de Tokyo', category: 'japanese')
